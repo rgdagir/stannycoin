d23f5dde784523aa678db8656893e0c9c2d6a80f
Andrew Tam <tam.andrew7@gmail.com>
2018-03-06 22:46:00 -0800

    Test token first

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
+
 const StannyCoin = artifacts.require("./StannyCoin.sol")
 const StannyCoinCrowdsale = artifacts.require("./StannyCoinCrowdsale.sol");
 
 module.exports = function(deployer, network, accounts) {
-  const rate = new web3.BigNumber(1000);
-  const wallet = accounts[0];
-  const token = StannyCoin;
-  deployer.deploy(StannyCoinCrowdsale, rate, wallet, token);
+  // const rate = new web3.BigNumber(100);
+  // const wallet = accounts[0];
+  // const token = StannyCoin;
+  // deployer.deploy(StannyCoinCrowdsale, 100000, wallet, token);
+  deployer.deploy(StannyCoin);
 }
